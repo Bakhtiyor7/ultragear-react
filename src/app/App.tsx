b8c69d362429d82b88857f79e727131c1178938e
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-10-25 19:35:47 +0900

    feat develop header navbar home component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { Box, Typography, Container, Stack, Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "../css/App.css";
+import "../css/navbar.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Dishes from "./components/dishes";
-import Users from "./components/users";
+
 import { RestaurantPage } from "./screens/RestaurantPage/indes";
 import { CommunityPage } from "./screens/CommunityPage";
 import { OrdersPage } from "./screens/OrdersPage/indes";
@@ -11,13 +11,25 @@ import { MemberPage } from "./screens/MemberPage/indes";
 import { HelpPage } from "./screens/HelpPage/indes";
 import { LoginPage } from "./screens/LoginPage/indes";
 import { HomePage } from "./screens/Homepage/indes";
+import { NavbarHome } from "./components/header";
+import { NavbarRestaurant } from "./components/header/restaurant";
+import { NavbarOthers } from "./components/header/others";
 
 function App() {
+  const [path, setPath] = useState();
+  const main_path = window.location.pathname;
+
   return (
-    <div>
-      <Router>
-        <div>
-          <nav>
+    <Router>
+      {main_path == "/" ? (
+        <NavbarHome setPath={setPath} />
+      ) : main_path.includes("/restaurant") ? (
+        <NavbarRestaurant />
+      ) : (
+        <NavbarOthers />
+      )}
+
+      {/* <nav>
             <ul>
               <li>
                 <Link to="/restaurant">RestaurantPage</Link>
@@ -41,34 +53,32 @@ function App() {
                 <Link to="/">Homepage</Link>
               </li>
             </ul>
-          </nav>
+          </nav> */}
 
-          <Switch>
-            <Route path="/restaurant">
-              <RestaurantPage />
-            </Route>
-            <Route path="/community">
-              <CommunityPage />
-            </Route>
-            <Route path="/orders">
-              <OrdersPage />
-            </Route>
-            <Route path="/member-page">
-              <MemberPage />
-            </Route>
-            <Route path="/help">
-              <HelpPage />
-            </Route>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <Route path="/">
-              <HomePage />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+      <Switch>
+        <Route path="/restaurant">
+          <RestaurantPage />
+        </Route>
+        <Route path="/community">
+          <CommunityPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <MemberPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
