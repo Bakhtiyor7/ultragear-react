8b7cf3f891c1e606b94261bc6693ceacca592e2b
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-12-21 23:29:00 +0900

    fix: CommunityPage develop chat based on client socket.io

--- src/lib/Definer.ts
+++ src/lib/Definer.ts
@@ -1,6 +1,7 @@
 export const Definer = {
-  input_err1: "att: please fulfill all inputs",
+  input_err1: "att: please fill all inputs",
   input_err2: "att: please insert only in JPG, JPEG, PNG formats!",
+  input_err3: "att: message input is empty!",
   general_err1: "att: something went wrong!",
   auth_err1: "att: please login first!",
 };
